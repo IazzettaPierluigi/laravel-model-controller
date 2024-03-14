ce24b1d44c8a6c6f9e2cfbee86c926de46a1212b
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-03-14 17:29:57 +0100

    imported database in controller (dd in welcome gives error)

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+//use app models movie viene aggiunta con l'estensione quando ho scritto movie a riga 15 (altrimenti va scritto a mano)
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
+        //recupero i records dal database (questa è una query, come quelle usate su mamp)
+        $movies = Movie::all();
+        //dopo ciò, fare il compact di movies
 
-
-        return view('pages.welcome');
+        return view('pages.welcome', compact('movies'));
     }
 }
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,5 +6,8 @@
 @section('main')
     <main>
         <h1>main welcome</h1>
+
+{{dd ($movies)}}
+
     </main>
 @endsection
